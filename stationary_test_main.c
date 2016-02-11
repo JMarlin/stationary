6aba7465c0d42a7261e514821e34da1baf86200b
jmarlin <j.marlin@outlook.com>
2016-02-11 16:24:46 -0500

    Attempting to turn FPS on

--- stationary_test_main.c
+++ stationary_test_main.c
@@ -1,9 +1,10 @@
-extern long GPUtest(void);
+#include "externals.h"
 
 int main(int argc, char* argv[]) {
 	
 	unsigned long disp;
 	
+	iShowFPS = 1;
 	GPUinit();
 	GPUopen(&disp, "Stationary", "");
 	GPUdisplayText("You are now running Stationary");
