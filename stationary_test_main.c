f36f953ffc7868b3ba9df694a286dc84f37b8124
jmarlin <j.marlin@outlook.com>
2016-02-17 17:10:18 -0500

    Fixed width of pixel data in fill command and changed color to blue

--- stationary_test_main.c
+++ stationary_test_main.c
@@ -60,7 +60,7 @@ void S_upload_image_data(unsigned long* src, unsigned short x, unsigned short y,
 	    GPUwriteData(src[i]);
 }
 
-void S_clear_framebuffer(unsigned long val) {
+void S_clear_framebuffer(unsigned short val) {
 	
 	int pixel_count = (1024 * 1024) >> 1;
 	int i;
@@ -122,7 +122,7 @@ int main(int argc, char* argv[]) {
     printf("The GPU has the following status: 0x%08lx\n", (unsigned long)GPUreadStatus());
     S_do_gpu_startup();
     printf("The GPU has the following status: 0x%08lx\n", (unsigned long)GPUreadStatus());
-	S_clear_framebuffer(0x00FF);
+	S_clear_framebuffer(0xFF00);
 	//S_draw_tri(1, 1, 319, 239, 1, 239, 0xFF, 0x00, 0x00);
 	//S_draw_tri(1, 1, 319, 1, 319, 239, 0x00, 0xFF, 0x00);
     //S_draw_tri_textured(0, 240, 160, 0, 320, 240, 0xFF, 0xFF, 0xFF);
