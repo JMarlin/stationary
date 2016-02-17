8af4a17315560ad96c9bd7dab211afe3bd7809bb
jmarlin <j.marlin@outlook.com>
2016-02-17 12:20:14 -0500

    Made sure GPUreadStatus() is being cast to an unsigned long

--- stationary_test_main.c
+++ stationary_test_main.c
@@ -55,9 +55,9 @@ int main(int argc, char* argv[]) {
 	GPUdisplayText("You are now running Stationary");
 	updateDisplay();
 	printf("You are now running stationary on display %lu\n", disp);
-    printf("The GPU has the following status: 0x%08lx\n", GPUreadStatus());
+    printf("The GPU has the following status: 0x%08lx\n", (unsigned long)GPUreadStatus());
     S_do_gpu_startup();
-    printf("The GPU has the following status: 0x%08lx\n", GPUreadStatus());
+    printf("The GPU has the following status: 0x%08lx\n", (unsigned long)GPUreadStatus());
     S_draw_tri(0, 200, 100, 0, 200, 200, 0x7F, 0x00, 0x00);
     updateDisplay();
     while(1); 
