d3ae36277727c47fd4e578df08fe752909e14224
jmarlin <j.marlin@outlook.com>
2016-02-19 14:29:06 -0500

    Made sure object.h includes all function declarations

--- object.h
+++ object.h
@@ -11,14 +11,17 @@ typedef struct Object {
     float z;
 } Object;
 
+
 void Object_delete(Object *object);
 int Object_add_triangle(Object* object, Vertex *v1, Vertex *v2, Vertex *v3);
 Object *Object_new();
+void Object_translate(Object* object, float x, float y, float z);
 void Object_rot_x_global(Object* object, float angle);
 void Object_rot_y_global(Object* object, float angle);
 void Object_rot_z_global(Object* object, float angle);
 void Object_rot_x_local(Object* object, float angle);
 void Object_rot_y_local(Object* object, float angle);
 void Object_rot_z_local(Object* object, float angle);
+
 
 #endif //OBJECT_H
