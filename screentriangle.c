5a65206aa0807afe03ed0ea313a11b51f83d2bae
jmarlin <j.marlin@outlook.com>
2016-02-22 09:34:53 -0500

    Made sorting z-depth the average between the nearest and furthest z-values in the polygon

--- screentriangle.c
+++ screentriangle.c
@@ -14,14 +14,29 @@ void ScreenTriangle_delete(ScreenTriangle* screen_triangle) {
 
 ScreenTriangle* ScreenTriangle_new(ScreenPoint *p1, ScreenPoint *p2, ScreenPoint *p3, color24 c) {
 	
-	float avg_z;
+	float avg_z, min_z, max_z;
 	ScreenTriangle *screen_triangle = (ScreenTriangle*)malloc(sizeof(ScreenTriangle));
 	
 	if(!screen_triangle)
 	    return screen_triangle;
 	
-	//Unrolled 'find max' loop 	
-	avg_z = (p1->z + p2->z + p3->z)/3;
+	max_z = p1->z;
+	
+	if(p2->z > max_z)
+	    max_z = p2->z;
+		
+	if(p3->z > max_z)
+	    max_z = p3->z;
+		
+	min_z = p1->z;
+	
+	if(p2->z < min_z)
+	    min_z = p2->z;
+		
+	if(p3->z < min_z)
+	    min_z = p3->z;
+	 	
+	avg_z = (min_z + max_z)>>2;
 		 
 	screen_triangle->points[0].x = p1->x;
 	screen_triangle->points[0].y = p1->y;
